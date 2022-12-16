56904d95d2c8aacf031b00376467cc216de47959
ASUS <Yehor Kholodiuk>
2022-12-16 18:40:08 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [likes, setLikes] = useState(1);
+  const [value, setValue] = useState('Input text')
+  function increment () {
+    setLikes(likes + 1)
+  }
+  function decrement(){
+    setLikes(likes - 1)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>{likes}</h1>
+      <h1>{value}</h1>
+      <input
+       type="text"
+       value={value}
+       onChange={event => setValue(event.target.value)}
+
+      />
+      <button onClick={increment}>+</button>
     </div>
   );
 }
